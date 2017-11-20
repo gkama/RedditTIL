6d04caa2b1ddbef6abb09ff1eb53dd367414266e
gogyprogramming <gkamacharov@gmail.com>
2017-11-20 11:45:34 -0500

    Error checking
    Added functionality to check if id exists in second dynamodb table

--- Function.cs
+++ Function.cs
@@ -24,15 +24,16 @@ namespace RedditTIL
         {
             TILs til = new TILs();
             List<TILs.children> children = await til.GetTILs(context);
-            string toReturn = JsonConvert.SerializeObject(children, Formatting.Indented);
-
-            //Log
-            context.Logger.Log(toReturn);
+            //string toReturn = JsonConvert.SerializeObject(children, Formatting.Indented);
 
             //Update DynamoDB with newest TILs
             string dynamodb_table = "reddit_til";
-            UpdateDynamoDB db = new UpdateDynamoDB(dynamodb_table, children);
-            await db.Update(context);
+            string id_table = "reddit_til_ids";
+            UpdateDynamoDB db = new UpdateDynamoDB(dynamodb_table, id_table, children);
+            string toReturn = await db.Update(context);
+
+            //Log
+            context.Logger.Log(toReturn);
 
             //Return Lambda
             return toReturn;
--- UpdateDynamoDB.cs
+++ UpdateDynamoDB.cs
@@ -13,68 +13,68 @@ namespace RedditTIL
     public class UpdateDynamoDB
     {
         public string table { get; set; }
+        public string id_table { get; set; }
         public List<TILs.children> Children { get; set; }
-        public UpdateDynamoDB(string table, List<TILs.children> Children)
+        public List<TILs.children> Added_Children { get; set; }
+        public UpdateDynamoDB(string table, string id_table, List<TILs.children> Children)
         {
             this.table = table;
+            this.id_table = id_table;
             this.Children = Children;
+            this.Added_Children = new List<TILs.children>();
         }
         public async Task<string> Update(ILambdaContext context)
         {
             var client = new AmazonDynamoDBClient();
             Table reddit_til_table = Table.LoadTable(client, table);
+            Table reddit_til_ids_table = Table.LoadTable(client, id_table);
 
             //Parse Children
             context.Logger.Log(string.Format("DynamoDB Table: {0}", reddit_til_table.TableName));
             context.Logger.Log(string.Format("Children size: {0}", this.Children.Count.ToString()));
             foreach (var children in this.Children)
             {
-                context.Logger.Log(string.Format("At Child: {0}", children.data.id));
-                var child = new Document();
-                child["id"] = children.data.id;
-                context.Logger.Log(children.data.id);
-                child["score"] = children.data.score;
-                context.Logger.Log(children.data.score.ToString());
-                child["title"] = children.data.title;
-                context.Logger.Log(children.data.title);
-                child["url"] = children.data.url;
-                context.Logger.Log(children.data.url);
-                child["subreddit"] = children.data.subreddit;
-                context.Logger.Log(children.data.subreddit);
-                child["thumbnail"] = children.data.thumbnail;
-                context.Logger.Log(children.data.thumbnail);
-                child["subreddit_id"] = children.data.subreddit_id;
-                context.Logger.Log(children.data.subreddit_id);
-                child["gilded"] = children.data.gilded;
-                context.Logger.Log(children.data.gilded.ToString());
-                child["name"] = children.data.name;
-                context.Logger.Log(children.data.name);
-                child["permalink"] = children.data.permalink;
-                context.Logger.Log(children.data.permalink);
-                child["link"] = children.data.link;
-                context.Logger.Log(children.data.link);
-                child["author"] = children.data.author;
-                context.Logger.Log(children.data.author);
-                child["ups"] = children.data.ups;
-                context.Logger.Log(children.data.ups.ToString());
-                child["downs"] = children.data.downs;
-                context.Logger.Log(children.data.downs.ToString());
-                child["num_comments"] = children.data.num_comments;
-                context.Logger.Log(children.data.num_comments.ToString());
+                //Check if id exists
+                Document getItem = await reddit_til_ids_table.GetItemAsync(children.data.id);
 
-                //Expression
-                Expression expr = new Expression();
-                expr.ExpressionStatement = "attribute_not_exists(id)";
-
-                PutItemOperationConfig config = new PutItemOperationConfig()
+                //If statement to check
+                if (getItem.Count == 0)
                 {
-                    // Optional parameter.
-                    ConditionalExpression = expr
-                };
+                    context.Logger.Log(string.Format("At Child: {0}", children.data.id));
+                    var child = new Document();
+                    child["id"] = children.data.id;
+                    child["score"] = children.data.score;
+                    child["title"] = children.data.title;
+                    child["url"] = children.data.url;
+                    child["subreddit"] = children.data.subreddit;
+                    child["thumbnail"] = children.data.thumbnail;
+                    child["subreddit_id"] = children.data.subreddit_id;
+                    child["gilded"] = children.data.gilded;
+                    child["name"] = children.data.name;
+                    child["permalink"] = children.data.permalink;
+                    child["link"] = children.data.link;
+                    child["author"] = children.data.author;
+                    child["ups"] = children.data.ups;
+                    child["downs"] = children.data.downs;
+                    child["num_comments"] = children.data.num_comments;
+                    child["last_updated"] = DateTime.Now;
 
-                //Insert item in dynamodb
-                Document x = await reddit_til_table.PutItemAsync(child, config);
-                context.Logger.LogLine("In method after put item async");
+                    //Id child
+                    var id_child = new Document();
+                    id_child["id"] = children.data.id;
+
+                    //Insert item in dynamodb
+                    await reddit_til_table.PutItemAsync(child);
+                    await reddit_til_ids_table.PutItemAsync(id_child);
+
+                    //Keep track of added children
+                    Added_Children.Add(children);
+                    context.Logger.Log(string.Format("Added item: {0}", children.data.id));
+                }
+                else
+                {
+                    context.Logger.LogLine(string.Format("ID: {0} already exists in table: {1}", children.data.id, this.table));
+                }
             }
             return "success";
         }
